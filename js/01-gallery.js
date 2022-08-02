058efa57eb4c8cf0f90315821051849a159505d2
USAenko <serhiiusenkoa@gmail.com>
2022-08-02 17:46:00 +0300

    18

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryElements = galleryItems.map(({preview, original, description}) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+    </div>`
+}).join('');
+
+gallery.insertAdjacentHTML("afterbegin", galleryElements);
+
+
